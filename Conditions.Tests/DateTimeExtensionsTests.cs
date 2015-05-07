4a09723e95bcb80c5e8eed0a7d0471a0d2a0f5fb
Michal Wereda <mwereda@future-processing.com>
2015-05-07 15:43:41 +0200

    IsGreaterThan for DateTime

---
+++ Conditions.Tests/DateTimeExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace Conditions.Tests
+{
+    public class DateTimeExtensionsTests
+    {
+        [Fact]
+        public void IsGreaterThan_GivenGreaterDateTime_ReturnsFalse()
+        {
+            var currentDateTime = DateTime.Now;
+
+            Assert.False(currentDateTime.IsGreaterThan(currentDateTime.AddSeconds(1)));
+            Assert.False(currentDateTime.IsGreaterThan(currentDateTime.AddMinutes(1)));
+            Assert.False(currentDateTime.IsGreaterThan(currentDateTime.AddHours(1)));
+        }
+
+        [Fact]
+        public void IsGreaterThan_GivenLowerDateTime_ReturnsTrue()
+        {
+            var currentDateTime = DateTime.Now;
+
+            Assert.True(currentDateTime.IsGreaterThan(currentDateTime.AddSeconds(-1)));
+            Assert.True(currentDateTime.IsGreaterThan(currentDateTime.AddMinutes(-1)));
+            Assert.True(currentDateTime.IsGreaterThan(currentDateTime.AddHours(-1)));
+        }
+
+        [Fact]
+        public void IsGreaterThan_GivenTheSameDateTime_ReturnsFalse()
+        {
+            var currentDateTime = DateTime.Now;
+
+            Assert.False(currentDateTime.IsGreaterThan(currentDateTime));
+        }
+    }
+}
---
+++ Conditions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Conditions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Checks if date time is greater than reference date time.
+        /// </summary>        
+        /// <param name="referenceDateTime">Reference date time.</param>
+        /// <returns>Boolean value whether date time is greater than reference date time.</returns>
+        public static bool IsGreaterThan(this DateTime dateTime, DateTime referenceDateTime)
+        {
+            return dateTime > referenceDateTime;
+        }
+    }
+}

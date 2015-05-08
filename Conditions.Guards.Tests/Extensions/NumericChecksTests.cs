09e171e0df7e67eea2439825d29302d04333bcbc
Michal Wereda <mwereda@future-processing.com>
2015-05-08 21:52:25 +0200

    Guards for numerics

---
+++ Conditions.Guards.Tests/Extensions/NumericChecksTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Conditions.Guards.Extensions;
+using Xunit;
+
+namespace Conditions.Guards.Tests.Extensions
+{
+    public class NumericChecksTests
+    {
+        [Theory]
+        [InlineData(31)]
+        [InlineData(10)]
+        public void IsGreaterThan_IntValueIsLowerOrEqualGivenValue_ThrowsArgumentException(int givenValue)
+        {
+            const int value = 10;
+
+            Assert.Throws<ArgumentException>(() => Check.If(value).IsGreaterThan(givenValue));
+        }
+
+        [Theory]
+        [InlineData(12.20)]
+        [InlineData(11.99)]
+        [InlineData(11.01)]
+        public void IsGreaterThan_DecimalValueIsLowerOrEqualGivenValue_ThrowsArgumentException(decimal givenValue)
+        {
+            const decimal value = 11m;
+
+            Assert.Throws<ArgumentException>(() => Check.If(value).IsGreaterThan(givenValue));
+        }
+
+        [Fact]
+        public void IsGreaterOrEqual_IntValueIsLower_ThrowsArgumentException()
+        {
+            const int value = 10;
+            const int minimalValue = 12;
+
+            Assert.Throws<ArgumentException>(() => Check.If(value).IsGreaterOrEqual(minimalValue));
+        }
+
+        [Fact]
+        public void IsGreaterOrEqual_DecimalValueIsLower_ThrowsArgumentException()
+        {
+            const decimal value = 100m;
+            const decimal minimalValue = 100.01m;
+
+            Assert.Throws<ArgumentException>(() => Check.If(value).IsGreaterOrEqual(minimalValue));
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(9)]
+        public void IsLowerThan_IntHigherOrEqual_ThrowsArgumentException(int givenValue)
+        {
+            const int value = 10;
+
+            Assert.Throws<ArgumentException>(() => Check.If(value).IsLowerThan(givenValue));
+        }
+
+        [Theory]
+        [InlineData(10.0)]
+        [InlineData(9.5)]
+        public void IsLowerThan_DecimalHigherOrEqual_ThrowsArgumentException(decimal givenValue)
+        {
+            const decimal value = 10.0m;
+
+            Assert.Throws<ArgumentException>(() => Check.If(value).IsLowerThan(givenValue));
+        }
+
+        [Fact]
+        public void IsLowerOrEqual_IntValueIsGreater_ThrowsArgumentException()
+        {
+            const int value = 12;
+            const int maximalValue = 10;
+
+            Assert.Throws<ArgumentException>(() => Check.If(value).IsLowerOrEqual(maximalValue));
+        }
+
+        [Fact]
+        public void IsLowerOrEqual_DecimalValueIsGreater_ThrowsArgumentException()
+        {
+            const decimal value = 100.02m;
+            const decimal maximalValue = 100.01m;
+
+            Assert.Throws<ArgumentException>(() => Check.If(value).IsLowerOrEqual(maximalValue));
+        }
+
+        [Fact]
+        public void IsBetween_IntNotInRange_ThrowsArgumentException()
+        {
+            const int value = 10;
+            const int minimalValue = 11;
+            const int maximalValue = 29;
+
+            Assert.Throws<ArgumentException>(() => Check.If(value).IsBetween(minimalValue, maximalValue));
+        }
+
+        [Fact]
+        public void IsBetween_DecimalNotInRange_ThrowsArgumentException()
+        {
+            const decimal value = 10.1m;
+            const decimal minimalValue = 10.11m;
+            const decimal maximalValue = 29m;
+
+            Assert.Throws<ArgumentException>(() => Check.If(value).IsBetween(minimalValue, maximalValue));
+        }
+    }
+}
---
+++ Conditions.Guards/Extensions/NumericChecks.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Conditions.Guards.Extensions
+{
+    public static class NumericChecks
+    {
+        /// <summary>
+        /// Guards against values lower or equal than given value.
+        /// </summary>                
+        /// <param name="givenValue">Given value.</param>        
+        public static void IsGreaterThan<T>(this If<T> ifObject, T givenValue) where T : struct, IComparable<T>
+        {
+            if (ifObject.Value.IsLowerOrEqual(givenValue))
+            {
+                throw new ArgumentException(string.Format("Value should be greater than {0}.", givenValue),
+                    ifObject.ParamName);
+            }
+        }
+
+        /// <summary>
+        /// Guards against values lower than given value.
+        /// </summary>               
+        /// <param name="minimalValue">Expected minimal value.</param>
+        public static void IsGreaterOrEqual<T>(this If<T> ifObject, T minimalValue) where T : struct, IComparable<T>
+        {
+            if (ifObject.Value.IsLowerThan(minimalValue))
+            {
+                throw new ArgumentException(string.Format("Value should be greater than or equal {0}.", minimalValue),
+                    ifObject.ParamName);
+            }
+        }
+
+        /// <summary>
+        /// Guards against values higher than or equal given value.
+        /// </summary>                
+        /// <param name="givenValue">Given value.</param>
+        public static void IsLowerThan<T>(this If<T> ifObject, T givenValue) where T : struct, IComparable<T>
+        {
+            if (ifObject.Value.IsGreaterOrEqual(givenValue))
+            {
+                throw new ArgumentException(string.Format("Value should be lower than {0}.", givenValue),
+                    ifObject.ParamName);
+            }
+        }
+
+        /// <summary>
+        /// Guards against values greater than maximal value.
+        /// </summary>                
+        /// <param name="maximalValue">Maximal value.</param>
+        public static void IsLowerOrEqual<T>(this If<T> ifObject, T maximalValue) where T : struct, IComparable<T>
+        {
+            if (ifObject.Value.IsGreaterThan(maximalValue))
+            {
+                throw new ArgumentException(string.Format("Value should be lower than or equal {0}.", maximalValue),
+                    ifObject.ParamName);
+            }
+        }
+
+        /// <summary>
+        /// Guards against values not in given range.
+        /// </summary>
+        /// <param name="minimalValue">Minmal expected value.</param>
+        /// <param name="maximalValue">Maximal expected value.</param>
+        public static void IsBetween<T>(this If<T> ifObject, T minimalValue, T maximalValue)
+            where T : struct, IComparable<T>
+        {
+            if (!ifObject.Value.IsBetween(minimalValue, maximalValue))
+            {
+                throw new ArgumentException(
+                    string.Format("Value should be in range ({0},{1}", minimalValue, maximalValue), ifObject.ParamName);
+            }
+        }
+    }
+}

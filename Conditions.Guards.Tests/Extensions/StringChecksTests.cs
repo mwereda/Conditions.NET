850d3bdf1288bcbbc32bbd21963e7851651bb3a0
Michal Wereda <mwereda@future-processing.com>
2015-05-08 20:40:21 +0200

    First guards for strings

---
+++ Conditions.Guards.Tests/Extensions/StringChecksTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Conditions.Guards.Extensions;
+using Xunit;
+
+namespace Conditions.Guards.Tests.Extensions
+{
+    public class StringChecksTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void IsNotNullOrEmpty_StringIsNullOrEmpty_ThrowsArgumentNullException(string invalidString)
+        {
+            Assert.Throws<ArgumentNullException>(() => Check.If(invalidString).IsNotNullOrEmpty());
+        }        
+
+        [Fact]
+        public void IsGuid_StringIsInvalidGuid_ThrowsArgumentException()
+        {
+            const string invalidGuid = "1234vcxvxcv34324";
+
+            Assert.Throws<ArgumentException>(() => Check.If(invalidGuid).IsGuid());
+        }
+
+        [Fact]
+        public void IsEmailAddress_StringIsInvalidEmailAddress_ThrowsArgumentException()
+        {
+            const string invalidEmailAddress = "mail.example.com";
+
+            Assert.Throws<ArgumentException>(() => Check.If(invalidEmailAddress).IsEmailAddress());
+        }
+
+        [Fact]
+        public void HasLength_StringHasNotExpectedLength_ThrowsArgumentException()
+        {
+            const string sampleString = "sample";
+            const int expectedLength = 3;
+
+            Assert.Throws<ArgumentException>(() => Check.If(sampleString).HasLength(expectedLength));
+        }
+    }
+}
--- Conditions.Guards/Extensions/StringChecks.cs
+++ Conditions.Guards/Extensions/StringChecks.cs
@@ -1,14 +1,52 @@
 ﻿using System;
 
-namespace Conditions.Guards
+namespace Conditions.Guards.Extensions
 {
     public static class StringChecks
     {
+        /// <summary>
+        /// Guards against null or empty string.
+        /// </summary>        
         public static void IsNotNullOrEmpty(this If<string> ifObject)
         {
             if (string.IsNullOrEmpty(ifObject.Value))
             {
                 throw new ArgumentNullException(ifObject.ParamName);
+            }
+        }
+
+        /// <summary>
+        /// Guards against string that is not GUID.
+        /// </summary>        
+        public static void IsGuid(this If<string> ifObject)
+        {
+            if (!ifObject.Value.IsGuid())
+            {
+                throw new ArgumentException("String is not valid GUID.", ifObject.ParamName);
+            }
+        }
+
+        /// <summary>
+        /// Guards against string that is not valid email address.
+        /// </summary>        
+        public static void IsEmailAddress(this If<string> ifObject)
+        {
+            if (!ifObject.Value.IsEmailAddress())
+            {
+                throw new ArgumentException("String is not valid email address.", ifObject.ParamName);
+            }
+        }
+
+        /// <summary>
+        /// Guards against string that's length is not expected.
+        /// <param name="expectedLength">Expected length.</param>
+        /// </summary>        
+        public static void HasLength(this If<string> ifObject, int expectedLength)
+        {
+            if (!ifObject.Value.HasLength(expectedLength))
+            {
+                throw new ArgumentException(
+                    string.Format("String doesn't have expected length ({0} chars)", expectedLength), ifObject.ParamName);
             }
         }
     }

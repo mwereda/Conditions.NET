b42914393803c52f971b3e2ec956dfe2f57b3213
Wereda, Michal <mwereda@future-processing.com>
2015-05-12 15:56:43 +0200

    IsGreaterThan check for DateTime

---
+++ Conditions.Guards.Tests/Extensions/DateTimeChecksTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace Conditions.Guards.Tests.Extensions
+{
+	public class DateTimeChecksTests
+	{
+		[Fact]
+		public void IsGreaterThan_DateTimeIsLowerOrEqualReferenceDate_ThrowsArgumentException()
+		{
+			var currentDateTime = DateTime.Now;
+
+			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime));
+			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddDays(-1)));
+			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddMilliseconds(-1)));
+			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddHours(-1)));
+			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddMinutes(-1)));
+			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddSeconds(-1)));
+		}
+	}
+}

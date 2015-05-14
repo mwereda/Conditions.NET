ed05dd5571893b98c65b0461add228a4cda8c513
Michal Wereda <mwereda@future-processing.com>
2015-05-14 15:11:43 +0200

    IsNull and IsNotNull checks + unit tests

---
+++ Conditions.Tests/ObjectExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+
+namespace Conditions.Tests
+{
+    public class ObjectExtensionsTests
+    {
+        [Fact]
+        public void IsNull_NullObject_ReturnsTrue()
+        {
+            DummyClass dummy= null;
+            
+            Assert.True(dummy.IsNull());
+        }
+
+        [Fact]
+        public void IsNull_NotNullObject_ReturnsFalse()
+        {
+            DummyClass dummy = new DummyClass();
+
+            Assert.False(dummy.IsNull());
+        }
+
+        [Fact]
+        public void IsNotNull_NullObject_ReturnsFalse()
+        {
+            DummyClass dummy = null;
+
+            Assert.False(dummy.IsNotNull());
+        }
+
+        [Fact]
+        public void IsNotNull_NotNullObject_ReturnsTrue()
+        {
+            DummyClass dummy = new DummyClass();
+
+            Assert.True(dummy.IsNotNull());
+        }
+
+        class DummyClass
+        {
+             
+        }
+    }
+}
---
+++ Conditions/ObjectExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Conditions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Checks if object is null.
+        /// </summary>        
+        /// <returns>Boolean value whether object is null.</returns>
+        public static bool IsNull(this Object @object)
+        {
+            return @object == null;
+        }
+
+        /// <summary>
+        /// Checks if object is not null.
+        /// </summary>        
+        /// <returns>Boolean value whether object is not null.</returns>
+        public static bool IsNotNull(this Object @object)
+        {
+            return !IsNull(@object);
+        }
+    }
+}

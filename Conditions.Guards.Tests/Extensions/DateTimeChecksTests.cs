7f4aefdccef2560cc96e88effc5437d40de9514d
Michal Wereda <mwereda@future-processing.com>
2015-05-14 22:17:00 +0200

    Fixed unit test for DateTime.IsGreaterThan

--- Conditions.Guards.Tests/Extensions/DateTimeChecksTests.cs
+++ Conditions.Guards.Tests/Extensions/DateTimeChecksTests.cs
@@ -11,11 +11,11 @@ namespace Conditions.Guards.Tests.Extensions
 			var currentDateTime = DateTime.Now;
 
 			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime));
-			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddDays(-1)));
-			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddMilliseconds(-1)));
-			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddHours(-1)));
-			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddMinutes(-1)));
-			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddSeconds(-1)));
+			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddDays(1)));
+			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddMilliseconds(1)));
+			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddHours(1)));
+			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddMinutes(1)));
+			Assert.Throws<ArgumentException>(() => Check.If(currentDateTime).IsGreaterThan(currentDateTime.AddSeconds(1)));
 		}
 	}
 }

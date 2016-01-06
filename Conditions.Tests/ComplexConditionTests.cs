a1f2316d0793c3b778645e49a2a1f6ebeee68996
Michal Wereda <mi.wereda@gmail.com>
2016-01-06 10:33:36 +0100

    Couple of more complex conditions tests (#4).

--- Conditions.Tests/ComplexConditionTests.cs
+++ Conditions.Tests/ComplexConditionTests.cs
@@ -19,5 +19,43 @@ namespace Conditions.Tests
 
             conditionResult.Result.ShouldBe(expectedResult);
         }
+
+        [Theory]
+        [InlineData(5, 14, false)]
+        [InlineData(10, 12, false)]
+        [InlineData(23, 2, true)]
+        [InlineData(0, 10, true)]
+        [InlineData(0, 11, false)]
+        public void IntegerHigherThanGivenValueOrLowerThanGivenValue_ReturnsExpectedResult(int lower, int higher, bool expectedResult)
+        {
+            int value = 11;
+
+            var conditionResult = value.IsLowerThan(lower).Or.IsGreaterThan(higher);
+
+            conditionResult.Result.ShouldBe(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(11)]
+        [InlineData(99)]
+        public void IntegerIsPositiveAndGreaterThanValueAndLowerThan100_ReturnsTrue(int givenValue)
+        {
+            var conditionResult = givenValue.IsPositive().And.IsGreaterThan(1).And.IsLowerThan(100);
+
+            conditionResult.Result.ShouldBeTrue();
+        }
+
+        [Theory]
+        [InlineData(-3)]
+        [InlineData(0)]
+        [InlineData(11)]
+        [InlineData(999)]
+        public void IntegerIsPositiveAndGreaterThanValueOrLowerThan100_ReturnsTrue(int givenValue)
+        {
+            var conditionResult = givenValue.IsPositive().And.IsGreaterThan(1).Or.IsLowerThan(100);
+
+            conditionResult.Result.ShouldBeTrue();
+        }
     }
 }
